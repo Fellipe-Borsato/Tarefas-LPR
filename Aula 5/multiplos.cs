9933f75b69cf413d6909153259e501f3bea7fb22
Fellipe-Borsato <fellipeborsato@gmail.com>
2024-03-19 22:50:02 -0300

    Create multiplos.cs

---
+++ Aula 5/multiplos.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program {
+    static void Main() {
+        Console.WriteLine("Digite o primeiro número:");
+        int num1 = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Digite o segundo número:");
+        int num2 = int.Parse(Console.ReadLine());
+
+        if (num1 % num2 == 0 || num2 % num1 == 0) 
+        {
+            Console.WriteLine($"{num1} e {num2} são Múltiplos");
+        } 
+        
+        else
+        {
+            Console.WriteLine($"{num1} e {num2} não são Múltiplos");
+        }
+    }
+}
