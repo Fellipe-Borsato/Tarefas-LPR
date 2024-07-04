79e98a33745cf4485f232a4341d89ef680570b61
Fellipe-Borsato <fellipeborsato@gmail.com>
2024-07-04 14:52:30 -0300

    Create exercício1.cs

---
+++ Aula 10/exercício1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+class HelloWorld
+{
+    static void Main()
+    {
+        List<string> nomes = new();
+        int numElmentos;
+        int cont = 1;
+
+        numElmentos = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < numElmentos; i++) //adiciona elementos a lista nomes
+        {
+            String Unome = Console.ReadLine();
+            nomes.Add(Unome);
+        }
+
+
+        List<string> ordem = new List<int>();
+
+        foreach (var Unome in nomes)
+        {
+            int tamanho = Unome.Length;
+            if (tamanho < 100)
+            {
+                ordem[tamanho].Add(Unome);
+            }
+        }
+    }
+}
+
