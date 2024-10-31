6f5cb3a7f28ee74e1dd016d12d82fab25c3a7f70
Fellipe-Borsato <fellipeborsato@gmail.com>
2024-10-31 15:06:37 -0300

    exercício 3

---
+++ Aula 11/Exercício 3/Aula 11 prática/Alunos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_11_prática
+{
+    internal class Alunos
+    {
+        string nome;
+        short matricula;
+        static double nota1, nota2, notaTrabalho, mediaNotas;
+
+        public void SetNota1(double valor) { nota1 = valor; }
+        public void SetNota2(double valor){nota2 = valor;}
+        public void SetNotaTrabalho(double valor){notaTrabalho = valor;}
+
+        public double Media(){return (nota1 + nota2) / 2;}
+    }
+}
---
+++ Aula 11/Exercício 3/Aula 11 prática/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Aula_11_prática
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Alunos aluno = new Alunos();
+
+            aluno.SetNota1(12.5);
+            aluno.SetNota2(7.0);
+            aluno.SetNotaTrabalho(9.0);
+
+            Console.WriteLine("A media de notas das provas é: " + aluno.Media());
+        }
+    }
+}
